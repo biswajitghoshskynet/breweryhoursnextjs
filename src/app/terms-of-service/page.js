da4177276f6dd38c1e3000b9faa986d8b4ac1f7d
Babai <biswajit@skynetonline.info>
2024-05-14 12:47:48 +0530

    14-5 1st

--- src/app/terms-of-service/page.js
+++ src/app/terms-of-service/page.js
@@ -16,17 +16,17 @@ export default function Page() {
                 <div className="container">
 
                     <h2 className="text-uppercase">Terms of Service</h2>
-                    <p class="tg-last-updated-date">Last updated: Oct 6, 2022 10:04 PM</p><p>Please read this Terms of Service ("Terms", "Terms of Service") carefully before using the Brewery Hours Application (the "Application") operated by Brewery Hours, a(n) Corporation formed in Georgia, United States  ("us","we","our") as this Terms of Service contains important information regarding limitations of our liability. Your access to and use of this Application is conditional upon your acceptance of and compliance with these Terms. These Terms apply to everyone, including but not limited to visitors, users and others, who wish to access and use the Application.</p>
+                    <p className="tg-last-updated-date">Last updated: Oct 6, 2022 10:04 PM</p><p>Please read this Terms of Service ("Terms", "Terms of Service") carefully before using the Brewery Hours Application (the "Application") operated by Brewery Hours, a(n) Corporation formed in Georgia, United States  ("us","we","our") as this Terms of Service contains important information regarding limitations of our liability. Your access to and use of this Application is conditional upon your acceptance of and compliance with these Terms. These Terms apply to everyone, including but not limited to visitors, users and others, who wish to access and use the Application.</p>
 
                     <p>By accessing or using the Application, you agree to be bound by these Terms. If you disagree with any part of the Terms, then you do not have our permission to access or use the Application.</p>
 
-                    <h2 class="text-center text-uppercase">Accounts</h2>
+                    <h2 className="text-center text-uppercase">Accounts</h2>
 
                     <p>When you create an account on our Application, you guarantee that you are 18 years of age or older and that the information that you provide us is accurate, complete, and current at all times. Inaccurate, incomplete, or obsolete information may result in the immediate termination of your account on the Application.</p>
 
                     <p>You are responsible for maintaining the confidentiality of your account and password, including but not limited to the restriction of access to your computer and/or account. You agree to accept responsibility for any and all activities or actions that occur under your account and/or password, whether your password is with our Application or a third party service. You must notify us immediately upon becoming aware of any breach of security or unauthorized use of your account.</p>
 
-                    <p>We reserve the right to terminate your account anytime at our sole discretion. You can terminate your account by contacting us.</p> <h2 class="text-center text-uppercase">Comments and uploading content</h2><p>By submitting a comment or uploading content onto this Application, you grant Brewery Hours a worldwide, non-exclusive, perpetual, royalty-free license to reproduce, publish and distribute the comment or content. When you make a comment or upload any content onto this Application, you agree that such comment or content may be viewed by other parties and it is your responsibility to ensure that the comment or content does not contain any confidential or proprietary information. You are also responsible for ensuring that your content or comment does not violate any laws, rules or regulations. We reserve the right to remove any content or comment at any time in our sole discretion.</p>
+                    <p>We reserve the right to terminate your account anytime at our sole discretion. You can terminate your account by contacting us.</p> <h2 className="text-center text-uppercase">Comments and uploading content</h2><p>By submitting a comment or uploading content onto this Application, you grant Brewery Hours a worldwide, non-exclusive, perpetual, royalty-free license to reproduce, publish and distribute the comment or content. When you make a comment or upload any content onto this Application, you agree that such comment or content may be viewed by other parties and it is your responsibility to ensure that the comment or content does not contain any confidential or proprietary information. You are also responsible for ensuring that your content or comment does not violate any laws, rules or regulations. We reserve the right to remove any content or comment at any time in our sole discretion.</p>
 
                     <p>The following is a non-exhaustive list of types of content or comments that you are prohibited from posting on our Application:</p>
                     <ul>
@@ -42,7 +42,7 @@ export default function Page() {
                         <li>Content whose purpose is spamming others.</li>
                     </ul>
 
-                    <h2 class="text-center text-uppercase">Prohibited uses</h2>
+                    <h2 className="text-center text-uppercase">Prohibited uses</h2>
 
                     <p>You agree that you will use this Application in accordance with all applicable laws, rules, regulations and these Terms at all times. The following is a non-exhaustive list of prohibited uses of this Application. You agree that you will not perform any of the following prohibited uses:</p>
 
@@ -60,21 +60,21 @@ export default function Page() {
                         <li>Using the Application in any way that violates any applicable federal, state or local laws, rules or regulations.</li>
                     </ol>
 
-                    <h2 class="text-center text-uppercase"><span class="text-uppercase">No warranty on Application</span></h2>
+                    <h2 className="text-center text-uppercase"><span className="text-uppercase">No warranty on Application</span></h2>
 
-                    <p class="text-uppercase">This Application is provided “as is,” No warranty, express or implied (including any implied warranty of merchantability, of satisfactory quality or fitness for a particular purpose or use) shall apply to this Application, whether arising by law, course of dealing, course of performance, usage of trade or otherwise.</p>
+                    <p className="text-uppercase">This Application is provided “as is,” No warranty, express or implied (including any implied warranty of merchantability, of satisfactory quality or fitness for a particular purpose or use) shall apply to this Application, whether arising by law, course of dealing, course of performance, usage of trade or otherwise.</p>
 
-                    <h2 class="text-center text-uppercase">Availability, errors and inaccuracies</h2>
+                    <h2 className="text-center text-uppercase">Availability, errors and inaccuracies</h2>
 
                     <p>We assume no liability for the availability, errors or inaccuracies of the information, products or services provided on this Application. We may experience delays in updating information on this Application and in our advertising on other websites. The information, products and services found on the Application may contain errors or inaccuracies or may not be complete or current. Products or services may be incorrectly priced or unavailable. We expressly reserve the right to correct any pricing errors on our Application. The inclusion or offering of any product or service on this Application does not constitute an endorsement or recommendation of such product or service by us. </p>
 
-                    <h2 class="text-center text-uppercase"><span class="text-uppercase">Damages and limitation of liability</span></h2>
+                    <h2 className="text-center text-uppercase"><span className="text-uppercase">Damages and limitation of liability</span></h2>
 
-                    <p class="text-uppercase">In no event shall Brewery Hours be liable for any direct, indirect, punitive, incidental, special or consequential damages arising out of, relating to or in any way connected with your access to, display of or use of this Application or with the delay or inability to access, display or use this Application, including but not limited to your reliance upon opinions or information appearing on this Application; any computer viruses, information, software, linked websites operated by third parties, products or services obtained through this Application, whether based on a theory of negligence, contract, tort, strict liability, consumer protection statutes or otherwise, even if Brewery Hours has been advised of the possibility of such damages. </p><h2 class="text-center text-uppercase">Links to third party websites</h2>
+                    <p className="text-uppercase">In no event shall Brewery Hours be liable for any direct, indirect, punitive, incidental, special or consequential damages arising out of, relating to or in any way connected with your access to, display of or use of this Application or with the delay or inability to access, display or use this Application, including but not limited to your reliance upon opinions or information appearing on this Application; any computer viruses, information, software, linked websites operated by third parties, products or services obtained through this Application, whether based on a theory of negligence, contract, tort, strict liability, consumer protection statutes or otherwise, even if Brewery Hours has been advised of the possibility of such damages. </p><h2 className="text-center text-uppercase">Links to third party websites</h2>
 
                     <p>This Application may contain hyperlinks to websites operated by third parties and not by us. We provide such hyperlinks for your reference only. We do not control such websites and are not responsible for their contents or the privacy or other practices of such websites. Further, it is your responsibility to take precautions to ensure that whatever links you click on or software that you download, whether from this Application or other websites or applications, is free of such items as viruses, worms, trojan horses, defects and other items of a destructive nature. Our inclusion of hyperlinks to such websites does not imply any endorsement of the material on such websites or any association with their operators. </p>
 
-                    <h2 class="text-center text-uppercase">Intellectual property and DMCA notice and procedure for intellectual property infringement claims</h2>
+                    <h2 className="text-center text-uppercase">Intellectual property and DMCA notice and procedure for intellectual property infringement claims</h2>
 
                     <p>All contents of this Application are ©2019 - 2024 Brewery Hours or third parties. All rights reserved. Unless specified otherwise, this Application and all content and other materials on this Application including but not limited to all logos, designs, text, graphics, pictures, information, data, software, sound files and arrangement thereof (collectively, “Content”) are the proprietary property of Brewery Hours and are either registered trademarks, trademarks or otherwise protected intellectual property of Brewery Hours or third parties in the United States and/or other countries.</p>
 
@@ -95,23 +95,23 @@ export default function Page() {
 
                     <p>You may submit your claim to us by contacting us at:</p>
                     <p>Brewery Hours<br />Jason Bass<br />info@breweryhours.com<br />(302) 503-2337<br />73 Silverbell Ln, Sharpsburg, GA 30277<br />
-                        <span class="text-uppercase">United States</span></p>
+                        <span className="text-uppercase">United States</span></p>
 
-                    <h2 class="text-center text-uppercase">Governing law, severability, dispute resolution and venue</h2>
+                    <h2 className="text-center text-uppercase">Governing law, severability, dispute resolution and venue</h2>
 
-                    <p>These Terms shall be governed and construed in accordance with the laws of the state of Georgia, United States, without regard to its conflict of laws provisions. <span class="text-uppercase">These terms shall not be governed by the United Nations convention on contracts for the sale of international goods, the Uniform Commercial Code, nor Incoterms</span>.</p>
+                    <p>These Terms shall be governed and construed in accordance with the laws of the state of Georgia, United States, without regard to its conflict of laws provisions. <span className="text-uppercase">These terms shall not be governed by the United Nations convention on contracts for the sale of international goods, the Uniform Commercial Code, nor Incoterms</span>.</p>
 
                     <p>Our failure to enforce any right or provision of these Terms will not be considered a waiver of that right or provision. If any provision of these Terms is held to be invalid or unenforceable by a court, the remaining provisions of these Terms will remain in effect. These Terms constitute the entire agreement between you and us regarding our Application, and supersede and replace any prior agreements we might have had with you regarding the Application. </p>
 
                     <p>Any controversy or claim arising out of or relating to these Terms including but not limited to the interpretation or breach thereof shall be resolved in a court of competent jurisdiction in Coweta County, Georgia.</p>
 
-                    <p class="text-uppercase">You and Brewery Hours agree that each may bring claims against the other only in your or its individual capacity and not as a plaintiff or class member in any class or representative action.</p>
+                    <p className="text-uppercase">You and Brewery Hours agree that each may bring claims against the other only in your or its individual capacity and not as a plaintiff or class member in any class or representative action.</p>
 
-                    <h2 class="text-center text-uppercase">Changes to Terms of Service</h2>
+                    <h2 className="text-center text-uppercase">Changes to Terms of Service</h2>
 
                     <p>We reserve the right to make changes to these Terms of Service at any time. We will not provide you with any notice when we make changes to this Terms of Service. </p>
 
-                    <h2 class="text-center text-uppercase">Questions</h2><p>If you have any questions about our Terms of Service, please contact us at info@breweryhours.com.</p>
+                    <h2 className="text-center text-uppercase">Questions</h2><p>If you have any questions about our Terms of Service, please contact us at info@breweryhours.com.</p>
 
 
 
